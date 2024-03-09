808359e032180ba070c04caa2f52d1dec45af2d5
imgres <92025754+imgres2002@users.noreply.github.com>
2024-03-09 02:02:26 +0100

    Add files via upload

--- pythonProject/statystyki.py
+++ pythonProject/statystyki.py
@@ -1,6 +1,6 @@
 import laspy
-import open3d as o3d
 import numpy as np
+
 # Wczytanie pliku LAS
 las_file_path = './WMII_CLASS.las'
 las = laspy.read(las_file_path)
@@ -16,3 +16,4 @@ print(f"sklasyfikowane punkty: {np.unique(las.classification)}")
 print(f"Zakres X: min = {las.header.x_min}, max = {las.header.x_max}")
 print(f"Zakres Y: min = {las.header.y_min}, max = {las.header.y_max}")
 print(f"Zakres Z: min = {las.header.z_min}, max = {las.header.z_max}")
+print(f"Intensywność: min = {np.min(las.intensity)}, max = {np.max(las.intensity)}, średnia = {np.mean(las.intensity)}")
