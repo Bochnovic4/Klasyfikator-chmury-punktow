ca32c5aadea2ec19b48879792a5fa866c8934d74
Lukasz <bochnolukasz@gmail.com>
2024-03-21 11:41:06 +0100

    Code cleanup

--- model.py
+++ model.py
@@ -8,14 +8,15 @@ class CloudPointClassifier:
     def __init__(self):
         self.model = RandomForestClassifier()
 
-    def classify_las(self, df):
-        predictions = self.model.predict(df.drop(columns=['label']))
+    def classify(self, points):
+        df = pd.DataFrame(points, columns=['x', 'y', 'z'])
+        predictions = self.model.predict(df)
         return predictions
 
     def train_model(self, features, labels):
         features = features.apply(pd.to_numeric, errors='coerce')
         X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
-        self.model = RandomForestClassifier(n_estimators=100, random_state=42)
+        self.model = RandomForestClassifier(n_estimators=100, random_state=42, n_jobs=-1)
         self.model.fit(X_train, y_train)
         return self.model
 
