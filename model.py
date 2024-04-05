e4255a3527b452db3ec08c567de5e284bd562d51
jakub-wroblewski <92018407+jakub-wroblewski@users.noreply.github.com>
2024-04-05 10:07:30 +0200

    Update for uniform distribution

--- model.py
+++ model.py
@@ -14,16 +14,16 @@ class CloudPointClassifier:
 
     def classify(self, points):
         df = pd.DataFrame(points, columns=COLUMNS)
-        df = df.apply(pd.to_numeric, errors='coerce')
-        df = self.transformer.transform(df)
+        # df = df.apply(pd.to_numeric, errors='coerce')
+        # df = self.transformer.transform(df)
         predictions = self.model.predict(df)
         return predictions
 
     def train_model(self, features, labels):
         features = pd.DataFrame(features, columns=COLUMNS)
         features = features.apply(pd.to_numeric, errors='coerce')
-        
-        self.transformer = QuantileTransformer(output_distribution='uniform', random_state=42)
+
+        self.transformer = QuantileTransformer(output_distribution='uniform')
         features = self.transformer.fit_transform(features)
 
         X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
@@ -32,7 +32,7 @@ class CloudPointClassifier:
         self.model.fit(X_train, y_train)
 
     def save(self, filename):
-        joblib.dump((self.model, self.transformer), filename)
+        joblib.dump(self.model, filename)
 
     def load(self, filename):
-        self.model, self.transformer = joblib.load(filename)
+        self.model = joblib.load(filename)
