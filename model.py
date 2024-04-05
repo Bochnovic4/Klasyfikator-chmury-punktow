20e05dc789f20d375d0496a57701ef448af5bbc6
jakub-wroblewski <92018407+jakub-wroblewski@users.noreply.github.com>
2024-04-05 09:16:28 +0200

    Update for uniform distribution

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
-from sklearn.model_selection import StratifiedShuffleSplit
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import QuantileTransformer
 import joblib
 
 from settings import N_JOBS, COLUMNS
@@ -10,28 +10,29 @@ from settings import N_JOBS, COLUMNS
 class CloudPointClassifier:
     def __init__(self):
         self.model = None
+        self.transformer = None
 
     def classify(self, points):
         df = pd.DataFrame(points, columns=COLUMNS)
+        df = df.apply(pd.to_numeric, errors='coerce')
+        df = self.transformer.transform(df)
         predictions = self.model.predict(df)
         return predictions
 
     def train_model(self, features, labels):
         features = pd.DataFrame(features, columns=COLUMNS)
         features = features.apply(pd.to_numeric, errors='coerce')
-        labels = pd.Series(labels)
+        
+        self.transformer = QuantileTransformer(output_distribution='uniform', random_state=42)
+        features = self.transformer.fit_transform(features)
 
-        splitter = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-        for train_index, test_index in splitter.split(features, labels):
-            X_train, X_test = features.iloc[train_index], features.iloc[test_index]
-            y_train, y_test = labels.iloc[train_index], labels.iloc[test_index]
+        X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
 
         self.model = RandomForestClassifier(n_estimators=100, random_state=42, n_jobs=N_JOBS)
         self.model.fit(X_train, y_train)
 
     def save(self, filename):
-        joblib.dump(self.model, filename)
+        joblib.dump((self.model, self.transformer), filename)
 
     def load(self, filename):
-        self.model = joblib.load(filename)
-
+        self.model, self.transformer = joblib.load(filename)
