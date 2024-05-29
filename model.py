bfd3fe6897377b55156458462aee855d204da379
imgres <92025754+imgres2002@users.noreply.github.com>
2024-05-29 20:24:08 +0200

    Update model features loading

--- model.py
+++ model.py
@@ -1,39 +1,44 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import balanced_accuracy_score, classification_report
 import joblib
 import numpy as np
-
-from settings import N_JOBS, COLUMNS, MAX_DEPTH
+from las_file_manager import LasFileManager
+import matplotlib.pyplot as plt
+from settings import N_JOBS, COLUMNS, MAX_DEPTH, N_ESTIMATORS
 
 
 class CloudPointClassifier:
     def __init__(self):
-        self.model = RandomForestClassifier(n_estimators=100, random_state=42, n_jobs=N_JOBS, max_depth=MAX_DEPTH)
+        self.model = RandomForestClassifier(n_estimators=N_ESTIMATORS, random_state=42, n_jobs=N_JOBS, max_depth=MAX_DEPTH)
         self.is_enabled = False
 
     def classify(self, las_file_manager):
-        (z,
-         cylinder_density, phi, theta,
-         min_height, max_height, mean_height) = las_file_manager.get_training_values()
+        values_dict = las_file_manager.get_model_values()
 
-        features = np.vstack((z,
-                              cylinder_density, phi, theta,
-                              min_height, max_height, mean_height)).T
-        features = pd.DataFrame(features, columns=COLUMNS)
+        features = pd.DataFrame(values_dict)
+        features = features.apply(pd.to_numeric, errors='coerce')
+
+        y_pred = self.model.predict(features)
+        y_test = las_file_manager.classes
+
+        acuracy = balanced_accuracy_score(y_test, y_pred)
+        print(f'Balanced accuracy: {acuracy:.2f}')
+        print(classification_report(y_test, y_pred))
         las_file_manager.classes = self.model.predict(features)
 
     def train_model(self, las_file_manager):
-        (z, intensity, ball_density,
-         cylinder_density, phi, theta,
-         min_height, max_height, mean_height) = las_file_manager.get_training_values(ground_classes=[2])
-        features = np.vstack((z, intensity, ball_density,
-                              cylinder_density, phi, theta,
-                              min_height, max_height, mean_height)).T
-        features = pd.DataFrame(features, columns=COLUMNS)
+        values_dict = las_file_manager.get_model_values(ground_classes=[2])
+        print("end")
+
+        features = pd.DataFrame(values_dict)
         features = features.apply(pd.to_numeric, errors='coerce')
-        x_train, x_test, y_train, y_test = train_test_split(features, las_file_manager.classes, test_size=0.2,
+        print("end")
+        x_train, x_test, y_train, y_test = train_test_split(features, las_file_manager.classes, test_size=0.20,
                                                             random_state=42)
+        print("end")
+
         self.model.fit(x_train, y_train)
         self.is_enabled = True
 
